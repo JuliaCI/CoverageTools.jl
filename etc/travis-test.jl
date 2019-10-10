8df6480458166fed901462b6c549d5c2f8375a8b
Dilum Aluthge <dilum@aluthge.com>
2019-10-09 20:33:57 -0400

    Reorder lines

--- etc/travis-test.jl
+++ etc/travis-test.jl
@@ -8,5 +8,5 @@ coveragecore_cov_res = CoverageCore.process_folder()
 Pkg.add("Coverage")
 import Coverage
 coverage_cov_res = Coverage.process_folder()
-# Coverage.Coveralls.submit(coverage_cov_res)
 Coverage.Codecov.submit(coverage_cov_res)
+# Coverage.Coveralls.submit(coverage_cov_res)
