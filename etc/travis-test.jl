436d053401e11d0d44ba7c3039385f55b0d9450b
Dilum Aluthge <dilum@aluthge.com>
2019-10-09 20:30:44 -0400

    Use Coverage.jl to submit coverage for this repo (CoverageCore.jl) to Coveralls and CodeCov (#7)
    * Use Coverage.jl to submit coverage for this repo (CoverageCore.jl) to Coveralls and CodeCov
    
    * Update travis-test.jl

--- etc/travis-test.jl
+++ etc/travis-test.jl
@@ -2,5 +2,11 @@ using Pkg
 Pkg.build()
 Pkg.test("CoverageCore"; coverage=true)
 
-using CoverageCore
-cov_res = process_folder()
+import CoverageCore
+coveragecore_cov_res = CoverageCore.process_folder()
+
+Pkg.add("Coverage")
+import Coverage
+coverage_cov_res = Coverage.process_folder()
+# Coverage.Coveralls.submit(coverage_cov_res)
+Coverage.Codecov.submit(coverage_cov_res)
