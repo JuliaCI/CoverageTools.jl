9a8b3d6c2258254d3e0b59d0a9a5f7f2ad3ec6e5
Dilum Aluthge <dilum@aluthge.com>
2019-10-11 17:38:28 -0400

    Retry submission up to 5 times (#18)

--- etc/travis-coverage.jl
+++ etc/travis-coverage.jl
@@ -1,9 +1,10 @@
 import CoverageCore
 coveragecore_cov_res = CoverageCore.process_folder()
 
-using Pkg
+import Pkg
 Pkg.add("Coverage")
 import Coverage
 coverage_cov_res = Coverage.process_folder()
-Coverage.Coveralls.submit(coverage_cov_res)
-Coverage.Codecov.submit(coverage_cov_res)
+
+retry(() -> Coverage.Coveralls.submit(coverage_cov_res); delays = ExponentialBackOff(n = 5))()
+retry(() -> Coverage.Codecov.submit(coverage_cov_res); delays = ExponentialBackOff(n = 5))()
